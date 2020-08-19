1fa6fdd3a911701512786f1253ec943ff93fe72f
Katsuma-F <k.fujita.indigo1003@gmail.com>
2020-08-19 17:36:03 +0900

    test commit

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', 'ArticleController@index');
+// Route::get('/', 'ArticleController@index');
