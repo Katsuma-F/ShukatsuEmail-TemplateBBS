e68526f4067148d70ca44abd620680ff480476da
Katsuma-F <k.fujita.indigo1003@gmail.com>
2020-08-22 13:43:25 +0900

    change: 記事モデルの作成&コントローラーを編集

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,45 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        // ダミーデータ
-        $articles = [
-            (object) [
-                'id' => 1,
-                'title' => 'タイトル１',
-                'body' => '本文１',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 1,
-                    'name' => 'ユーザー名１',
-                ],
-            ],
-            (object) [
-                'id' => 2,
-                'title' => 'タイトル２',
-                'body' => '本文２',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 2,
-                    'name' => 'ユーザー名２',
-                ],
-            ],
-            (object) [
-                'id' => 3,
-                'title' => 'タイトル３',
-                'body' => '本文３',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 3,
-                    'name' => 'ユーザー名３',
-                ],
-            ],
-        ];
+        $articles = Article::all()->sortByDesc('created_at');
 
         return view('articles.index', ['articles' => $articles]);
     }
