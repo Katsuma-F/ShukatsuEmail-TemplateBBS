4d1d23d929bfdcd15d990ab0ef46bc48527815ce
Katsuma-F <k.fujita.indigo1003@gmail.com>
2020-08-26 22:27:20 +0900

    add: 記事更新画面、記事削除モーダル画面の作成

---
+++ resources/views/articles/edit.blade.php
@@ -0,0 +1,26 @@
+@extends('app')
+
+@section('title', 'テンプレ更新')
+
+@include('nav')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+                <div class="card mt-3">
+                    <div class="card-body pt-0">
+                        @include('error_card_list')
+                        <div class="card-text">
+                            <form method="POST" action="{{ route('articles.update', ['article' => $article]) }}">
+                                @method('PATCH')
+                                @include('articles.form')
+                                <button type="submit" class="btn btn-block purple-gradient">更新する</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
