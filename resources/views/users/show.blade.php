be72b0a39e384dff469f866d9aefa52278d78964
Katsuma-F <k.fujita.indigo1003@gmail.com>
2020-08-29 17:42:56 +0900

    docs: 文言を変更 in users/show.blade.php

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -22,12 +22,12 @@
         <ul class="nav nav-tabs nav-justified mt-3">
             <li class="nav-item">
                 <a class="nav-link text-muted active" href="{{ route('users.show', ['name' => $user->name]) }}">
-                    記事
+                    テンプレ
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link text-muted" href="">
-                    お気に入り
+                    お気に入りテンプレ
                 </a>
             </li>
         </ul>
